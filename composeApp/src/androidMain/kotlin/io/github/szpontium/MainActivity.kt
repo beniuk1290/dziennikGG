8f5a2a6952e85ccdee678688a8e8a8ea59e149a6
beniuk1290 <dziennik.gg@int.pl>
2026-09-08 16:09:55 +0200

    Debug: Add crash handler to show error on screen

--- composeApp/src/androidMain/kotlin/io/github/szpontium/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/github/szpontium/MainActivity.kt
@@ -1,15 +1,31 @@
 package io.github.szpontium
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.ScrollView
+import android.widget.LinearLayout
+import android.graphics.Color
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import io.github.szpontium.session.initAndroidDataStoreContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        try { enableEdgeToEdge() } catch (_: Exception) {}
         super.onCreate(savedInstanceState)
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            val trace = throwable.stackTraceToString()
+            val msg = "${throwable.message}\n\n$trace"
+            runOnUiThread {
+                val tv = TextView(this).apply {
+                    text = msg
+                    setTextColor(Color.RED)
+                    setPadding(32, 32, 32, 32)
+                    textSize = 12f
+                }
+                val scroll = ScrollView(this).apply { addView(tv) }
+                setContentView(scroll)
+            }
+        }
         initAndroidDataStoreContext(this)
         setContent {
             App()
